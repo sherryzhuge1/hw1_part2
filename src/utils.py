c530636ceab498cdffacb2947de9ff12d27d4bda
Sherry Zhuge <szhuge@andrew.cmu.edu>
2025-02-04 22:15:27 -0500

    wandb added

--- src/utils.py
+++ src/utils.py
@@ -55,7 +55,7 @@ def load_config(config_path):
 def setup_wandb(config):
     wandb.login(key="fd2f4a747b6d6934bcc41013afb25a02f4753a3e")
     return wandb.init(
-        name    = "run-sherry", ### Wandb creates random run names if you skip this field, we recommend you give useful names
+        name    = "run-19", ### Wandb creates random run names if you skip this field, we recommend you give useful names
         reinit  = True, ### Allows reinitalizing runs when you re-run this cell
         # id     = "xgesgg8t", ### Insert specific run id here if you want to resume a previous run
         # resume = "must", ### You need this to resume previous runs, but comment out reinit = True when using this
